e018c56bb8276959d7028b9183a4848808b73b2a
notksjaay <ksjaay@gmail.com>
2023-04-09 15:54:04 +0100

    Moved components to own files

---
+++ components/index.js
@@ -0,0 +1,19 @@
+import {Check} from "./check.mdx";
+import {Cross} from "./cross.mdx";
+import {Highlight} from "./highlight.mdx";
+import {Image} from "./image.mdx";
+import {InlineImage} from "./inlineimage.mdx";
+import {PremiumIcon} from "./premiumicon.mdx";
+import {TitleImage} from "./titleimage.mdx";
+import {Underline} from "./underline.mdx";
+
+export {
+  Check,
+  Cross,
+  Highlight,
+  Image,
+  InlineImage,
+  PremiumIcon,
+  TitleImage,
+  Underline,
+};
